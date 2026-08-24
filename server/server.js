0318af7dfe573ef4b0d6d3732cfc6f3634c64694
Twisha Patel <twipatel2006@gmail.com>
2026-08-24 20:23:31 +0530

    setup server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "DevSpace server is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`DevSpace server running on port ${PORT}`);
+});
